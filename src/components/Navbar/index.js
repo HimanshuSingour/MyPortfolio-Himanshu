ccf2ec902ce98639b2a7a161e4b4699f26604a0b
HimanshuSingour <singourhimanshu3@gmail.com>
2024-05-11 20:45:17 +0530

    yes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,11 +11,12 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-          </a>
-        </NavLogo>
+      <NavLogo to='/'>
+  <a href="/" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20', cursor: 'pointer' }}>
+    <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+  </a>
+</NavLogo>
+
         <MobileIcon>
           <FaBars onClick={() => {
             setIsOpen(!isOpen)
